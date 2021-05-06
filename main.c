296b667347f66f2547d1fbb94089b127f2e426fd
JKMMS <jaquemaurermachado@gmail.com>
2021-05-06 19:10:39 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,14 +5,14 @@ int main (void){ //Funcção principal
 
   int x = 0, y = 0; //Declaração de duas variaveis inteiras chamadas x e y inicializadas em 0
 
-  printf("Digite um numero: ");
-  scanf("%i", &x);
+  printf("Digite um numero: "); //comentário de bosta
+  scanf("%i", &x); //comentário de bosta
 
-  printf("Digite outro numero: ");
-  scanf("%i", &y);
+  printf("Digite outro numero: "); //comentário de bosta
+  scanf("%i", &y); //comentário de bosta
 
-  printf("A soma de %i e %i resulta em %i.", x, y, x + y);
+  printf("A soma de %i e %i resulta em %i.", x, y, x + y); //comentário de bosta
 
-  return 0;
+  return 0; //comentário de bosta
 
 }
